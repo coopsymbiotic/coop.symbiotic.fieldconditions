73beb6f3c53e0598ea9abe07a59c46af6a2851ca
Mathieu Lutfy <mathieu@symbiotic.coop>
2019-11-19 09:27:28 -0500

    Update fieldconditions.php comments

--- fieldconditions.php
+++ fieldconditions.php
@@ -1,11 +1,12 @@
 <?php
 
 require_once 'fieldconditions.civix.php';
+use CRM_Fieldconditions_ExtensionUtil as E;
 
 /**
  * Implements hook_civicrm_config().
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/ 
  */
 function fieldconditions_civicrm_config(&$config) {
   _fieldconditions_civix_civicrm_config($config);
@@ -14,7 +15,7 @@ function fieldconditions_civicrm_config(&$config) {
 /**
  * Implements hook_civicrm_xmlMenu().
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
  */
 function fieldconditions_civicrm_xmlMenu(&$files) {
   _fieldconditions_civix_civicrm_xmlMenu($files);
@@ -23,7 +24,7 @@ function fieldconditions_civicrm_xmlMenu(&$files) {
 /**
  * Implements hook_civicrm_install().
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
  */
 function fieldconditions_civicrm_install() {
   _fieldconditions_civix_civicrm_install();
@@ -32,7 +33,7 @@ function fieldconditions_civicrm_install() {
 /**
  * Implements hook_civicrm_postInstall().
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
  */
 function fieldconditions_civicrm_postInstall() {
   _fieldconditions_civix_civicrm_postInstall();
@@ -41,7 +42,7 @@ function fieldconditions_civicrm_postInstall() {
 /**
  * Implements hook_civicrm_uninstall().
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
  */
 function fieldconditions_civicrm_uninstall() {
   _fieldconditions_civix_civicrm_uninstall();
@@ -50,7 +51,7 @@ function fieldconditions_civicrm_uninstall() {
 /**
  * Implements hook_civicrm_enable().
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function fieldconditions_civicrm_enable() {
   _fieldconditions_civix_civicrm_enable();
@@ -59,7 +60,7 @@ function fieldconditions_civicrm_enable() {
 /**
  * Implements hook_civicrm_disable().
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
  */
 function fieldconditions_civicrm_disable() {
   _fieldconditions_civix_civicrm_disable();
@@ -68,7 +69,7 @@ function fieldconditions_civicrm_disable() {
 /**
  * Implements hook_civicrm_upgrade().
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
  */
 function fieldconditions_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
   return _fieldconditions_civix_civicrm_upgrade($op, $queue);
@@ -80,7 +81,7 @@ function fieldconditions_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
  * Generate a list of entities to create/deactivate/delete when this module
  * is installed, disabled, uninstalled.
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
  */
 function fieldconditions_civicrm_managed(&$entities) {
   _fieldconditions_civix_civicrm_managed($entities);
@@ -93,7 +94,7 @@ function fieldconditions_civicrm_managed(&$entities) {
  *
  * Note: This hook only runs in CiviCRM 4.4+.
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
  */
 function fieldconditions_civicrm_caseTypes(&$caseTypes) {
   _fieldconditions_civix_civicrm_caseTypes($caseTypes);
@@ -107,7 +108,7 @@ function fieldconditions_civicrm_caseTypes(&$caseTypes) {
  * Note: This hook only runs in CiviCRM 4.5+. It may
  * use features only available in v4.6+.
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
  */
 function fieldconditions_civicrm_angularModules(&$angularModules) {
   _fieldconditions_civix_civicrm_angularModules($angularModules);
@@ -116,7 +117,7 @@ function fieldconditions_civicrm_angularModules(&$angularModules) {
 /**
  * Implements hook_civicrm_alterSettingsFolders().
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
  */
 function fieldconditions_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
   _fieldconditions_civix_civicrm_alterSettingsFolders($metaDataFolders);
@@ -127,7 +128,7 @@ function fieldconditions_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) 
 /**
  * Implements hook_civicrm_preProcess().
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
  *
 function fieldconditions_civicrm_preProcess($formName, &$form) {
 
@@ -136,14 +137,14 @@ function fieldconditions_civicrm_preProcess($formName, &$form) {
 /**
  * Implements hook_civicrm_navigationMenu().
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_navigationMenu
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
  *
 function fieldconditions_civicrm_navigationMenu(&$menu) {
-  _fieldconditions_civix_insert_navigation_menu($menu, NULL, array(
-    'label' => ts('The Page', array('domain' => 'coop.symbiotic.fieldconditions')),
-    'name' => 'the_page',
-    'url' => 'civicrm/the-page',
-    'permission' => 'access CiviReport,access CiviContribute',
+  _fieldconditions_civix_insert_navigation_menu($menu, 'Mailings', array(
+    'label' => E::ts('New subliminal message'),
+    'name' => 'mailing_subliminal_message',
+    'url' => 'civicrm/mailing/subliminal',
+    'permission' => 'access CiviMail',
     'operator' => 'OR',
     'separator' => 0,
   ));
