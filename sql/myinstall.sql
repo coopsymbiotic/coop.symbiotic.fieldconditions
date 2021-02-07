6ca069c2beee6cd8b503475cc444a9b2177ecba3
Mathieu Lutfy <mathieu@symbiotic.coop>
2021-02-07 10:51:09 -0500

    Fix install on MySQL, which frowns on text default values

--- sql/myinstall.sql
+++ sql/myinstall.sql
@@ -2,6 +2,6 @@ CREATE TABLE `civicrm_fieldcondition` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Field condition map',
   `type` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
   `name` varchar(255) COMMENT 'Map name for admins' COLLATE utf8_unicode_ci DEFAULT NULL,
-  `settings` text DEFAULT '' COLLATE utf8_unicode_ci,
+  `settings` text COLLATE utf8_unicode_ci,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
