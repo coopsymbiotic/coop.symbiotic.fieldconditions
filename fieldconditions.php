c7d9ea3a3ade3e1021942b9881e1058dc1c6451d
Mathieu Lutfy <mathieu@symbiotic.coop>
2021-02-11 20:00:44 +0000

    Adds support for Multiple Contact Update (batch update)

--- fieldconditions.php
+++ fieldconditions.php
@@ -154,6 +154,19 @@ function fieldconditions_civicrm_buildForm($formName, &$form) {
 
     foreach ($form->_elementIndex as $key => $val) {
       foreach ($settings['fields'] as $k2 => $field) {
+        // This is a horrible hack, but the Multiple Contact Update (Batch update) form names things differently.
+        if ($formName == 'CRM_Contact_Form_Task_Batch' && substr($key, 0, 6) == 'field[') {
+          $fix = [
+            'country_id' => 'country',
+            'county_id' => 'county',
+            'state_province_id' => 'state_province',
+          ];
+
+          if (isset($fix[$field['entity_field']])) {
+            $field['entity_field'] = $fix[$field['entity_field']];
+          }
+        }
+
         // There is a bit of twisted logic in here to handle multiple address records.
         // Addresses use as "hash_id" such as "1-2", where 1 is the map_id, 2 is the address ID.
         // We switch back and forth because a same form can have address and non-address fields, semi-randomly.
@@ -166,11 +179,28 @@ function fieldconditions_civicrm_buildForm($formName, &$form) {
           $maps[$hash_id]['fields'][$k2]['qf_field'] = $key;
         }
         elseif (preg_match('/^address\[(\d+)\]\[' . $field['entity_field'] . '(_[^\]]*)?\]$/', $key, $found)) {
+          // Ex: address fields on Edit Contact
           $hash_id = $map_id . '-' . $found[1];
           if (empty($maps[$hash_id])) {
             $maps[$hash_id] = $settings;
           }
           $maps[$hash_id]['fields'][$k2]['qf_field'] = 'address_' . $found[1] . '_' . $field['entity_field'] . (!empty($found[2]) ? $found[2] : '');
+        }
+        elseif ($formName == 'CRM_Contact_Form_Task_Batch' && preg_match('/^field\[(\d+)\]\[' . $field['entity_field'] . '(-[^\]]*)?\]$/', $key, $found)) {
+          // Ex: multiple contact update
+          $hash_id = $map_id . '-' . $found[1];
+          if (empty($maps[$hash_id])) {
+            $maps[$hash_id] = $settings;
+          }
+          $maps[$hash_id]['fields'][$k2]['qf_field'] = 'field_' . $found[1] . '_' . $field['entity_field'] . (!empty($found[2]) ? $found[2] : '');
+        }
+        elseif ($formName == 'CRM_Contact_Form_Task_Batch' && preg_match('/^field\[(\d+)\]\[address_' . $field['entity_field'] . '(-[^\]]*)?\]$/', $key, $found)) {
+          // Ex: multiple contact update, custom address fields
+          $hash_id = $map_id . '-' . $found[1];
+          if (empty($maps[$hash_id])) {
+            $maps[$hash_id] = $settings;
+          }
+          $maps[$hash_id]['fields'][$k2]['qf_field'] = 'field_' . $found[1] . '_address_' . $field['entity_field'] . (!empty($found[2]) ? $found[2] : '');
         }
         elseif (preg_match('/' . $field['entity_field'] . '_/', $key)) {
           // @todo This currently only matches against custom fields
