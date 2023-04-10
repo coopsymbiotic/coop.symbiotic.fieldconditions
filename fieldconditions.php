5a56d2e78762c203699f288e58b54f5931c3fb26
Mathieu Lutfy <mathieu@symbiotic.coop>
2023-04-10 14:00:02 +0000

    civix upgrade

--- fieldconditions.php
+++ fieldconditions.php
@@ -13,15 +13,6 @@ function fieldconditions_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
- */
-function fieldconditions_civicrm_xmlMenu(&$files) {
-  _fieldconditions_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
@@ -31,96 +22,12 @@ function fieldconditions_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function fieldconditions_civicrm_postInstall() {
-  _fieldconditions_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function fieldconditions_civicrm_uninstall() {
-  _fieldconditions_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function fieldconditions_civicrm_enable() {
   _fieldconditions_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function fieldconditions_civicrm_disable() {
-  _fieldconditions_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function fieldconditions_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _fieldconditions_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
- */
-function fieldconditions_civicrm_managed(&$entities) {
-  _fieldconditions_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
- */
-function fieldconditions_civicrm_caseTypes(&$caseTypes) {
-  _fieldconditions_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
- */
-function fieldconditions_civicrm_angularModules(&$angularModules) {
-  _fieldconditions_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function fieldconditions_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _fieldconditions_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
